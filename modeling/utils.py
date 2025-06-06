8366e3c36e74010fe47952ab9904bfc731c1ad44
Alexander Bösecke <boesi@users.noreply.github.com>
2025-06-06 11:50:35 +0200

    add a normalize parameter & fixed a small error

--- modeling/utils.py
+++ modeling/utils.py
@@ -1,7 +1,7 @@
 from sklearn.metrics import classification_report, ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
 
-def check_classification_binary(model, X_train, X_test, y_train, y_test):
+def check_classification_binary(model, X_train, X_test, y_train, y_test, normalize=None):
     """
     Evaluates the performance of a given model on training and testing datasets.
 
@@ -12,10 +12,16 @@ def check_classification_binary(model, X_train, X_test, y_train, y_test):
 
     Args:
         model: The trained machine learning model to evaluate.
-        train_x: Training features
-        test_x: Testing features
-        train_y: Training target
-        test_y: Testing target
+        X_train: Training features.
+        X_test: Testing features.
+        y_train: Training target.
+        y_test: Testing target.
+        normalize (str or None, optional): This parameter is passed directly to the `normalize` argument of `ConfusionMatrixDisplay.from_estimator`. It determines how the confusion matrix is normalized. Acceptable values are:
+            - None: No normalization is applied (counts are shown).
+            - 'true': Normalize over the true (rows).
+            - 'pred': Normalize over the predicted (columns).
+            - 'all': Normalize over the whole matrix (all values sum to 1).
+        See the scikit-learn documentation for more details.
 
     Visualization:
         - The function creates a 2x2 grid of subplots:
@@ -32,12 +38,15 @@ def check_classification_binary(model, X_train, X_test, y_train, y_test):
     classes = ['Terminated', 'Fulfilled']
     fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(12, 4), gridspec_kw={'height_ratios': [3, 1]})
 
-    cmd_test = ConfusionMatrixDisplay.from_estimator(model, X_test, y_test, display_labels=classes, cmap=plt.cm.Blues, ax=axes[0][0])
+    values_format = 'd'
+    if normalize != None:
+        values_format = '.3f'
+    cmd_test = ConfusionMatrixDisplay.from_estimator(model, X_test, y_test, display_labels=classes, normalize=normalize, values_format=values_format, cmap='Blues', ax=axes[0][0])
     axes[0][0].set_title('Test Data')
     axes[1][0].text(0, 0, classification_report(y_test, test_pred_y, target_names=classes, digits=3), verticalalignment='top', fontfamily='monospace')
     axes[1][0].axis('off')
 
-    cmd_test = ConfusionMatrixDisplay.from_estimator(model, X_train, y_train, display_labels=classes, cmap=plt.cm.Greens, ax=axes[0][1])
+    cmd_test = ConfusionMatrixDisplay.from_estimator(model, X_train, y_train, display_labels=classes, normalize=normalize, values_format=values_format, cmap='Greens', ax=axes[0][1])
     axes[0][1].set_title('Train Data')
     axes[1][1].text(0, 0, classification_report(y_train, train_pred_y, target_names=classes, digits=3), verticalalignment='top', fontfamily='monospace')
     axes[1][1].axis('off')
